e49e169b8715af5a1fca370a48d89a37ecb12b8d
Taiwo ODETOLA <taiwo.odetolla@gmail.com>
2020-11-21 15:47:51 +0100

    Updated - Added the owners section

--- 1_basic project_puppy_adoption/forms.py
+++ 1_basic project_puppy_adoption/forms.py
@@ -11,3 +11,9 @@ class DelForm(FlaskForm):
     
     id = IntegerField('Id number of Puppy to remove: ')
     submit = SubmitField('Remove Puppy')
+    
+class OwnerForm(FlaskForm):
+    
+    name = StringField('Name of Owner: ')
+    pup_id = IntegerField('Id of Puppy: ')
+    submit = SubmitField('Add Owner')
